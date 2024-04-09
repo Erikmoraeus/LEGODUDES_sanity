5f8344ffe911e5b457b330302ea6b1ebaa94247f
Erikmoraeus <erik.moraeus@gmail.com>
2024-04-09 13:55:47 +0200

    flyttet kortene til de ulike kategoriene de tilhører

--- frontend/sanity/services/categoryServices.js
+++ frontend/sanity/services/categoryServices.js
@@ -1,13 +1,29 @@
 import { client } from "../client";
 
 export async function fetchAllCategories() {
-    const data = await client.fetch(`*[_type == "categories"]{_id,categorytitle,"catslug":categoryurl.current}`)
+    const data = await client.fetch(`*[_type == "categories"]{
+        _id,
+        categorytitle,
+        "catslug":categoryurl.current}`)
     return data
 }
-
 
 export async function fetchCategoryBySlug(slug) {
-    const data = await client.fetch(`*[_type == "categories" && categoryurl.current == $slug]`, {slug})
+    const data = await client.fetch(`*[_type == "categories" && categoryurl.current == $slug]{
+        _id,
+        categorytitle,
+        "catProducts": *[_type == "products" && references(^._id)]{
+            _id,
+            productname,
+            "slug": producturl.current,
+            price,
+            stock,
+            "catname": category->categorytitle,
+            "catslug": category->categoryurl.current,
+            "image": productimage.asset->url
+        }
+    }`, {slug})
     return data
 }
-   
+
+//*[_type == "products" && category._ref == _id]
